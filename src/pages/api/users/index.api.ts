0ac542225b1ec09bdee1f4a9b0dc2afde0772659
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-06-19 18:02:34 -0300

    Manipulando cookies no Next

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/src/lib/prisma'
+import { setCookie } from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
@@ -6,16 +7,33 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   if (req.method !== 'POST') {
-    return res.status(485).end()
+    return res.status(405).end()
   }
 
   const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already exists',
+    })
+  }
 
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@call:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
